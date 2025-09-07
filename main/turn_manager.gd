0bd5f2c42f97f2414bf2f0e8760ddb2665862a23
pozbliz <sebro@hushmail.com>
2025-09-07 18:38:43 +0900

    feat: add turn manager to handle team and turn management

--- main/turn_manager.gd
+++ main/turn_manager.gd
@@ -4,8 +4,20 @@ extends Node
 var characters: Array[Character]
 var turn_order: Array[Character]
 var number_of_teams: int = 2
-var number_of_worms: int = 6
+var number_of_worms: int = 2
 
 
-func assign_to_team():
-	pass
+func assign_to_team() -> void:
+	SettingsManager.load_settings()
+	number_of_teams = SettingsManager.number_of_teams
+	number_of_worms = SettingsManager.number_of_worms
+	
+	assert (characters.size() == number_of_teams * number_of_worms)
+	
+	for i in range(characters.size()):
+		var team = i % number_of_teams
+		var chosen = characters[i]
+		chosen.team = team
+		chosen.add_to_group("team" + str(team))
+		
+	turn_order = characters
--- util/settings/settings.gd
+++ util/settings/settings.gd
@@ -2,17 +2,14 @@ extends Node
 
 
 var number_of_teams: int = 2
-var number_of_worms: int = 6
+var number_of_worms: int = 2
 
-
-func _ready() -> void:
-	load_settings()
 
 func load_settings() -> void:
 	var config = ConfigFile.new()
 	if config.load("user://options.cfg") == OK:
-		number_of_teams = config.get_value("gameplay", "number_of_teams", true)
-		number_of_worms = config.get_value("gameplay", "number_of_worms", true)
+		number_of_teams = config.get_value("gameplay", "number_of_teams", number_of_teams)
+		number_of_worms = config.get_value("gameplay", "number_of_worms", number_of_worms)
 
 func save_settings() -> void:
 	var config = ConfigFile.new()
